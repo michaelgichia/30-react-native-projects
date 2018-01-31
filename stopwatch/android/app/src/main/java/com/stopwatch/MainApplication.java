16470b964b0b1bc0ee261f18f26a07441785a4ad
michaelgichia <gichuru.gichia@gmail.com>
2018-01-31 09:12:53 +0300

    Add gradient linear package.

--- stopwatch/android/app/src/main/java/com/stopwatch/MainApplication.java
+++ stopwatch/android/app/src/main/java/com/stopwatch/MainApplication.java
@@ -3,6 +3,7 @@ package com.stopwatch;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -21,15 +22,14 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new LinearGradientPackage());
     }
 
     @Override
     protected String getJSMainModuleName() {
       return "index";
     }
+
   };
 
   @Override
